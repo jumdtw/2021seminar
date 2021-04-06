0a9e72cd33a541a6afbf24d3b9e45f90d86e2573
jumdtw <ttnmrm1m3k5g1m3r3k@gmail.com>
2021-04-06 13:05:32 +0900

    aaaa

--- seminar/randslice.py
+++ seminar/randslice.py
@@ -1,1 +1,25 @@
-import pandas
+import pandas as pd
+import csv 
+import sys
+import glob
+
+# python3 randslice.py savefilename 
+
+savefilename = "default.csv"
+path = ".\\data\\buf"
+
+files = glob.glob(path+"\\*")
+print(files)
+lst = []
+for file in files:
+    with open(file) as f:
+        a = [e for e in csv.reader(f)]
+        lst = lst + a
+df = pd.DataFrame(lst)
+
+if len(sys.argv)>1:
+    savefilename = sys.argv[1]
+    
+
+print(df)
+df.to_csv(".\\data\\"+savefilename)
