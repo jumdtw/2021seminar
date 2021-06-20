11aa82cd5b30dc84386b155fec52b8f01e1d2809
jumdtw <ttnmrm1m3k5g1m3r3k@gmail.com>
2021-06-21 08:36:47 +0900

    asdf

--- seminar/learn-judge.py
+++ seminar/learn-judge.py
@@ -2,7 +2,19 @@ from __future__ import print_function
 import sys
 import pandas as pd
 from sklearn import metrics
-from sklearn.tree import DecisionTreeClassifier
+#アルゴリズム用
+from sklearn.ensemble import VotingClassifier
+#from sklearn.ensemble import AdaBoostClassifier
+#from sklearn.ensemble import BaggingClassifier
+#from sklearn.ensemble import ExtraTreesClassifier
+#from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier
+#from sklearn.neighbors import KNeighborsClassifier
+#from sklearn.linear_model import LogisticRegression
+#from sklearn.naive_bayes import GaussianNB
+#from sklearn.svm import SVC
+#from sklearn.svm import LinearSVC
+#from sklearn.tree import DecisionTreeClassifier
 
 # python learn-judge.py learndata judgedata
 
@@ -57,12 +69,27 @@ def read_dataset(path, n=N):
 
 
 def main():
+
+    estimators = [
+            #('ada', AdaBoostClassifier()),
+            #('bag', BaggingClassifier()),
+            #('et', ExtraTreesClassifier()),
+            #('gb', GradientBoostingClassifier()),
+            ('rf', RandomForestClassifier(n_estimators=100)),
+            #('knn', KNeighborsClassifier()),
+            #('logit', LogisticRegression(solver='lbfgs', max_iter=10000)),
+            #('nb', GaussianNB()),
+            #('svm', SVC(gamma='scale', probability=True)),
+            #('linearSVC',LinearSVC()),
+            #('tree', DecisionTreeClassifier())
+    ]
+
     print('学習用データ')
     train, label_train = read_dataset(TRAIN_PATH)
     print('評価用データ')
     test, label_test = read_dataset(TEST_PATH)
 
-    clf = DecisionTreeClassifier()
+    clf = VotingClassifier(estimators)
     
     clf.fit(train, label_train)
     pred = clf.predict(test)
